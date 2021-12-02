7c1d71e3dc52cbb56cd2ac086676c048bf2cd781
Arthur Thouin <arthurthouin@hotmail.com>
2021-12-02 17:28:40 +0100

    header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Header.css'
+import Logo from '../assets/logo/logo_bleu_complet.png'
+
+function Header() {
+    return (
+        <div className="container">
+            <div className="menuGeneral">
+                <img src={Logo} alt="Logo"/> 
+                <div className="menuList">
+                    <ul>
+                        <li>
+                            <a href="#"><span>Accueil</span></a>
+                        </li>
+                        <li>
+                            <a href="#">Classement</a>
+                        </li>
+                        <li>
+                            <a href="#">Statistiques</a>
+                        </li>
+                        <li>
+                            <a href="#">Conseils</a>
+                        </li>
+                        <li>
+                            <a href="#">À propos</a>
+                        </li>
+                    </ul>
+                    
+                </div>
+                <div className="menuButtons">
+                        <button className="ButtonInscription">Inscription</button>
+                        <button className="ButtonConnexion">Connexion</button>
+                </div>
+            </div> 
+        </div>
+
+    );
+}
+
+export default Header;
