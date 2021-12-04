14a0cba9d572f29e4bce40fc0055ccd0e64897b0
zizo2k21 <yedjaziz@yahoo.fr>
2021-12-04 01:42:40 +0100

    debut page stat |:

--- src/components/pagestats/index.tsx
+++ src/components/pagestats/index.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import './style.css'
 
 function pagestats() {
     return (
-        
         <div>
-            Hello
+      <div className="cards">
+        <div className="card card-1">
+      
+      <h2 className="card__title">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+      
+    </div>
+    <div className="card card-1">
+      
+      <h2 className="card__title">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+      
+    </div>
+    <div className="card card-1">
+      
+      <h2 className="card__title">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+      
+    </div>  
+    <div className="card card-1">
+      
+      <h2 className="card__title">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+      
+    </div>
+        </div>
         </div>
     )
 }
