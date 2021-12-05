96f27c4dc0277a252323b60818829ade1efe4474
Arthur Thouin <arthurthouin@hotmail.com>
2021-12-05 04:01:18 +0100

    m

--- src/components/PageStats/MostPlayedChampions/MostPlayedChampions.tsx
+++ src/components/PageStats/MostPlayedChampions/MostPlayedChampions.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import './MostPlayedChampions.css'
+import Aatrox from '../../assets/images/Aatrox.png';
+import image26 from '../../assets/images/image26.png';
+import MaskGroup from '../../assets/images/MaskGroup.svg';
+
 
 function MostPlayedChampions() {
     return (
-        <div className="boxTitle titleMostPlayed">
-            MOST PLAYED CHAMPIONS
+        <div className="mostPlayedChampions">
+            <div className="boxTitle titleMostPlayed">
+                Most played champions
+            </div>
+            <div className="box boxMostPlayedChampions">
+                <div className="imgContainerMostPlayed">
+                    <div className="imgLeftPlayedChampion">
+                        <img src={Aatrox} className="aatrox"/>
+                        <span>Aatrox</span>
+                            
+                            <div className="textLeftMostPlayed">
+                            <img src={MaskGroup}/>
+                            <img src={image26} className="imgNumberOfTrophies"/><span>38 220</span>
+                        </div>
+                     </div>
+                </div>
+            </div>
         </div>
     )
 }
